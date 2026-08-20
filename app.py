cb2f799df0dfa82823c3b4b8b043c5b13c39c4e7
cjendres1 <cjendres1@gmail.com>
2026-08-19 23:05:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,11 +12,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 # Load spaCy lightweight model
 @st.cache_resource
 def load_nlp():
-    try:
-        return spacy.load("en_core_web_sm")
-    except OSError:
-        spacy.cli.download("en_core_web_sm")
-        return spacy.load("en_core_web_sm")
+    return spacy.load("en_core_web_sm")
 
 nlp = load_nlp()
 
